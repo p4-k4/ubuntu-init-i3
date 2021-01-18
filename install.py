fe9a46467292339ab0cad3b41a742261867dd3d3
p4-k4 <pauriniwiringi@gmail.com>
2021-01-18 22:59:49 +0000

    sync changes

--- install.py
+++ install.py
@@ -1,7 +1,18 @@
 import os
+from pathlib import Path
+
+user = os.environ['SUDO_USER'] if 'SUDO_USER' in os.environ else os.environ['USER']
+home = str(Path.home())
 
 os.system('apt update && apt upgrade -y')
-os.system('apt-get install i3 ubuntu-drivers-common mesa-utils mesa-tils-extra gnupg xautolock xorg xserver-xorg wget unzip wpasupplicant')
+os.system('apt-get install i3 gnome-terminal ubuntu-drivers-common mesa-utils mesa-tils-extra gnupg xautolock xorg xserver-xorg wget unzip wpasupplicant')
 
-os.system('apt-get install gnome-terminal -y')
+bash_profile = open(f'/home/{user}/.profile', 'a')
+xinitrc = open(f'/home/{user}/.xinitrc', 'a')
+chunk1 = '# Auto start i3\nif [[ -z $DISPLAY ]] && [[ $(tty) = /dev/tty1 ]]; then\n  startx\nfi'
+chunk2 = 'export TERMINAL = "gnome-terminal"\nalias open="xdg-open"\ni3'
+
+bash_profile.write(chunk1)
+xinitrc.write(chunk2)
+
 os.system('reboot now')
