f57c43fd60d30a76491b7943a781af629c0a0093
p4-k4 <pauriniwiringi@gmail.com>
2021-01-18 22:34:17 +0000

    tidy up

--- setup.sh
+++ setup.sh
@@ -5,18 +5,3 @@ apt-get install -y \
 python3 \
 
 chmod +x install.py && /usr/bin/python3 install.py
-
-#i3 \
-#ubuntu-drivers-common \
-#mesa-utils \
-#mesa-utils-extra \
-#gnupg \
-#numlockx \
-#xautolock \
-#xorg \
-#xserver-xorg \
-#wget \
-#unzip \
-#wpasupplicant
-
-
