acc11354fdb7938808d4d1add38bd47229154693
p4-k4 <pauriniwiringi@gmail.com>
2021-01-18 21:31:15 +0000

    added install.sh

---
+++ install.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+apt-get update
+
+apt-get install -y \
+python3-pip \
+git
+
+cd ~/ && git clone 
+
+#i3 \
+#ubuntu-drivers-common \
+#mesa-utils \
+#mesa-utils-extra \
+#gnupg \
+#numlockx \
+#xautolock \
+#xorg \
+#xserver-xorg \
+#wget \
+#unzip \
+#wpasupplicant
+
+
